ad16f632abb52d0983741cb7e991c3b4d90c2eb4
farzan <farzan@bettermode.com>
2025-12-18 01:59:49 +0330

    chore: revert changes
    This reverts commit 2b06bafd30c0752aa67c225e0a0de9f8031d3632, reversing
    changes made to fa10b45f5cd00cadb7285361b09115e61dcc903d.

--- src/data/posts.js
+++
@@ -1,6 +0,0 @@
-export const posts = [
-  { id: 1, title: "Getting Started", description: "Learn the basics of our platform." },
-  { id: 2, title: "Advanced Tips", description: "Take your skills to the next level." },
-  { id: 3, title: "Best Practices", description: "Follow these guidelines for success." },
-];
-
